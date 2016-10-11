17db74699ac629951f59d64cd751de030dd9ec98
YsenGrimm <kloeten.wars@googlemail.com>
2016-10-11 23:36:57 +0200

    updated typescript, new npm scripts

--- release.webpack.config.js
+++ release.webpack.config.js
@@ -3,26 +3,22 @@ var webpack = require("webpack");
 module.exports = {
     entry: "./src/intro.ts",
     output: {
-        filename: "./dist/game.js",
+        filename: "./release/game.js",
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
     },
 
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.ts$/, loader: "ts-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
         ]
     }
 };
